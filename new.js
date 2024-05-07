c1583e260236f8b08b89d47012be5ef7c31904c9
ainhoa <aino20m@gmail.com>
2024-05-07 23:33:11 +0200

    js commit

--- new.js
+++ new.js
@@ -1,1 +1,58 @@
 /*para empezar repo*/
+/*Switch Statement*/
+var dataPoint = {};
+
+switch (typeof dataPoint) {
+  case "string":
+    console.log("It's a string");
+    break;
+  case "number":
+    console.log("It's a number");
+    break;
+  case "boolean":
+    console.log("It's a boolean");
+    break;
+  default:
+    console.log('No matches');
+}
+
+
+
+function switchStatement() {
+    
+    var myData = 5;
+
+    switch (typeof myData) {
+        case "string":
+            console.log("string");
+            break;
+        case "number":
+            console.log ("number");
+            break;
+        default:
+            console.log("number");
+    }
+}
+switchStatement();
+
+function switchStatement() {
+    var result;
+    switch (typeof result) {
+        case "number":
+            result = "number";
+            break;
+        default:
+            result = "number"; 
+    }
+    return result;
+}
+
+switchStatement(); 
+
+
+
+
+
+
+
+
